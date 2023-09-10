7f76ad1ec3cc5fd4c24488905edf218f2e9070bc
Anshu Choudhary <anshuchoudhary@Anshus-MacBook-Air.local>
2023-09-11 03:45:35 +0530

    Adding Simple Form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import TicTacToe from './TicTacToe';
 
 function App() {
+  const [name, setName] = useState('Name');
+  const [age, setAge] = useState('Age');
+  const [email, setEmail] = useState('Email');
+  const [display, setDisplay]=useState('');
+
+  const handleChangeName = (e) => {
+    setName(e.target.value);
+  }
+  const handleChangeAge = (e) => {
+    setAge(e.target.value);
+  }
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value);
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setDisplay(name+', '+age+', '+email);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Tic Tac Toe</h1>
-        <TicTacToe />
-      </header>
+        <h1>Display Info</h1>
+        <form onSubmit={handleSubmit}>
+          <div>Name: 
+            <input type='text' onChange={handleChangeName} />
+          </div>
+          <div>Age:
+            <input type='text' onChange={handleChangeAge} />
+          </div>
+          <div>Email:
+            <input type='text' onChange={handleChangeEmail} />
+          </div>
+            <br></br>
+            <input type='submit' title='Show'/>
+            {display}
+        </form>
     </div>
   );
 }
